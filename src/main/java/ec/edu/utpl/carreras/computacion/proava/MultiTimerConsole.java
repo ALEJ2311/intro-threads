c797de63e71a1a029fe6dd88203cc00709cc35db
Jorge Lopez Vargas <jorgaf@gmail.com>
2025-04-22 12:46:01 -0500

    Para iniciar

--- src/main/java/ec/edu/utpl/carreras/computacion/proava/MultiTimerConsole.java
+++ src/main/java/ec/edu/utpl/carreras/computacion/proava/MultiTimerConsole.java
@@ -8,10 +8,5 @@ public class MultiTimerConsole {
         TimerTask timerA = new TimerTask("Temporizador A", 8);
         timerA.run();
         System.out.println();
-        TimerTask timerB = new TimerTask("Temporizador B", 5);
-        timerB.run();
-        System.out.println();
-        TimerTask timerC = new TimerTask("Temporizador C", 6);
-        timerC.run();
     }
 }
--- src/main/java/ec/edu/utpl/carreras/computacion/proava/TimerTask.java
+++ src/main/java/ec/edu/utpl/carreras/computacion/proava/TimerTask.java
@@ -12,11 +12,6 @@ public class TimerTask {
     public void run() {
         while(seconds > 0) {
             System.out.printf("%s: %d segundos restantes%n", name, seconds);
-            try {
-                Thread.sleep(1_000);
-            } catch (InterruptedException e) {
-                System.out.printf("%s interrumpido", name);
-            }
             seconds --;
         }
     }
